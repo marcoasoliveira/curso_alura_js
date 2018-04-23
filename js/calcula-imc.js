dc688e611986cc43a235902b7238b736a6e1d5a0
marcoasoliveira <crazymaker2039@gmail.com>
2018-04-23 00:02:31 -0300

    topico 6: validando form

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,8 +1,6 @@
 var titulo = document.querySelector('h1')
 titulo.textContent = "Aparecida Nutricionista" //muda o conteudo dentro da tag
 
-//Boas Práticas com Javascript
-
 var pacientes = document.querySelectorAll('.paciente')
 
 for(var i = 0; i < pacientes.length; i++){
@@ -15,27 +13,38 @@ for(var i = 0; i < pacientes.length; i++){
     var tdAtura = paciente.querySelector(".info-altura")
     var altura = tdAtura.textContent
 
-    var pesoValido = true
-    var alturaValida = true
-
     var tdImc = paciente.querySelector('.info-imc')
 
-    if (peso <= 0 || peso >= 1000){
+    if (!validaPeso(peso)){
         pesoValido = false
         tdImc.textContent = 'Peso Inválido'
         paciente.classList.add('paciente-invalido') //classList retorna as classes do elemento
     }                                               //.add adiciona uma classe nova
 
-    if (altura <= 0 || altura >= 3){
+    if (!validaAltura(altura)){
         alturaValida = false
         tdImc.textContent = 'Altura Inválida'
         paciente.classList.add('paciente-invalido')
     }
 
-    if (pesoValido && alturaValida) {
+    if (validaPeso(peso) && validaAltura(altura)) {
         var imc = calculaImc(peso,altura) //altura² == (altura*altura)
         tdImc.textContent = imc // define o numero de casas decimais
     }
+}
+
+function validaPeso(peso){
+    if(peso > 0 && peso < 500){
+        return true
+    }
+    return false
+}
+
+function validaAltura(altura){
+    if(altura > 0 && altura < 2.50){
+        return true
+    }
+    return false
 }
 
 function calculaImc (peso,altura){
--- js/form_5.js
+++ js/form_5.js
@@ -3,22 +3,42 @@ var botaoAdicionar = document.querySelector('#adicionar-paciente')
 botaoAdicionar.onclick = (event) => { // faz o mesmo que o addEventListener (event shortcut)
     event.preventDefault() //previne o comportamento padrão do botão, no caso, evita o refresh do botão
     var form = document.querySelector("#form-adiciona")
+    
+    //extraindo informações do paciente do form
+    // var nome = form.nome.value,
+    //     peso = form.peso.value,
+    //     altura = form.altura.value,
+    //     gordura = form.gordura.value
+    
     var paciente = obtemInformacaoDoFormulario(form)
+    console.log(paciente)
+
+    //cria a tr e a td do paciente
+    // var pacienteTr = document.createElement("tr")
+    // var nomeTd = document.createElement("td")
+    // var pesoTd = document.createElement("td")
+    // var alturaTd = document.createElement("td")
+    // var gorduraTd = document.createElement("td")
+    // var imcTd = document.createElement("td")
+
+    // nomeTd.textContent = paciente.nome
+    // pesoTd.textContent = paciente.peso
+    // alturaTd.textContent = paciente.altura
+    // gorduraTd.textContent = paciente.gordura
+    // imcTd.textContent = paciente.imc
+
+    // pacienteTr.appendChild(nomeTd)
+    // pacienteTr.appendChild(pesoTd)
+    // pacienteTr.appendChild(alturaTd)
+    // pacienteTr.appendChild(gorduraTd)
+    // pacienteTr.appendChild(imcTd)
     var pacienteTr = montaTr(paciente)
 
-    var erro = validaPaciente(paciente)
-
-    if(erro.length > 0){
-        exibeMensagensDeErro(erro)
-        return
-    }
-
+    // adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes")
     tabela.appendChild(pacienteTr)
 
     form.reset() //função que limpa do form.
-    var ul = document.querySelector("#mensagem-erro")
-    ul.innerHTML = ""
 }
 
 function obtemInformacaoDoFormulario(form){
@@ -29,12 +49,17 @@ function obtemInformacaoDoFormulario(form){
         gordura: form.gordura.value,
         imc: calculaImc(form.peso.value,form.altura.value) //função criada no arquivo calcula-imc.js
     }
+    
     return paciente
 }
 
 function montaTr(paciente){
     var pacienteTr = document.createElement("tr")
     pacienteTr.classList.add("paciente")
+
+    // var nomeTd = document.createElement("td")
+    // nomeTd.classList.add("info-nome") ---- a função montaTd substituiu a criação manual
+    // nomeTd.textContent = paciente.nome (... passo incluso dentro do montaTd)
 
     pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"))
     pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"))
@@ -51,24 +76,4 @@ function montaTd(dado,classe){
     td.classList.add(classe)
 
     return td
-}
-
-function validaPaciente(paciente){
-    var erros = []
-    if(paciente.nome.length == 0) erros.push("Nome não pode estar em branco")
-    if(!validaPeso(paciente.peso)) erros.push("Peso é inválido")
-    if(!validaAltura(paciente.altura)) erros.push("Altura é inválida")
-    if(paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco")
-    
-    return erros
-}
-
-function exibeMensagensDeErro(erros){
-    var ul = document.querySelector("#mensagem-erro")
-    ul.innerHTML = ""
-    erros.forEach((erro)=> {
-        var li = document.createElement("li")
-        li.textContent = erro
-        ul.appendChild(li)
-    });
 }
